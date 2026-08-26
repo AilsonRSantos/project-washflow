c623fbd36da91fcd802f27c77c3e81360e922d9e
AilsonRSantos <ailsonrosadossantos4@gmail.com>
2026-08-26 19:09:33 -0300

    feat: estrutura o layout do dashboard

--- src/components/dashcontainer/dashborddcontainer.jsx
+++ src/components/dashcontainer/dashborddcontainer.jsx
@@ -1,15 +1,13 @@
-import "./dashbord.css"
+import "./dashbord.css";
 
 const DashbordContainer = () => {
-
   return (
     <div className="dashbord">
-      <div className="dashbord-header">
-        <h1 className="dashbord-tittle"> DashBord</h1>
-        <p>Visão geral do seu negócio</p>
-      </div>
-
       <div className="dashbord-cards">
+        <div className="dashbord-card">
+          <h2 className="card-tittle">Faturamento</h2>
+          <span className="card-value">R$ 8.540,00</span>
+        </div>
         <div className="dashbord-card">
           <h2 className="card-tittle"> Clientes</h2>
           <span className="card-value">120</span>
@@ -23,19 +21,26 @@ const DashbordContainer = () => {
           <h2 className="card-tittle">Serviços</h2>
           <span className="card-value">245</span>
         </div>
-        <div className="dashbord-card">
-          <h2 className="card-tittle">Faturamento</h2>
-          <span className="card-value">R$ 8.540</span>
-        </div>
       </div>
 
       <div className="dashbord-content">
         <div className="dashbord-panel">
-          <h2 className="panel-tittle">Faturmento</h2>
+          <h2 className="panel-tittle">Faturamento</h2>
         </div>
 
         <div className="dashbord-pnel">
-          <h2 className="panel-tittle"> Serviços realizados</h2>
+          <h2 className="panel-tittle"> Serviços realizados mais realizados</h2>
+          <div className="dashbord-pnel-content">
+            <div className="pie-chart"> 
+              <h2>circulo</h2>
+            </div>
+              <div className="high-turnover-services">
+                <p>serviço 1</p>
+                <p>serviço 2</p>
+                <p>serviço 3</p>
+                <p>serviço 4</p>
+              </div>
+          </div>
         </div>
       </div>
 
